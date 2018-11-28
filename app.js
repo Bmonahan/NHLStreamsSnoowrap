acd9e5d6d62ec7596529c8496417f965eb0df03e
Brysen Monahan <monahan.brysen@gmail.com>
2018-11-28 15:22:39 -0500

    Added config file for snoowrap

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
 'use strict'
 const snoowrap = require('snoowrap');
+const env = process.env.NODE_ENV || 'dev';
+const config = require('./config')[env];
 
 const r = new snoowrap({
-  userAgent: "DLCOnoobGhost",
-  clientId: "NQ_Q0Jcy_KstnQ",
-  clientSecret: "6HJJAPfbr7C3VLoG7jwXbWH2n0I",
-  refreshToken: "70808853-dMtva1BwQ-TU4a9KL1YboRARXuE"
+  userAgent: config.userAgent,
+  clientId: config.clientId,
+  clientSecret: config.clientSecret,
+  refreshToken: config.refreshToken
 })
-// r.getSubmission('9pubaq').title.then(console.log);
-
-// r.getSubreddit('nhlstreams').getNew().map(post => post.id).then((value)=>{
-//   for(let i = 0;i<value.length;i++){
-//     r.getSubmission(value[i]).title.then((title)=>{
-//       if(title.includes("NHL")){
-//         console.log(`Title: ${title}`);
-//       }
-//     });
-    
-//   }
-// });
-
 r.search({
   query: 'Game Thread',
   subreddit: 'nhlstreams',
-  sort: 'new'
+  sort: 'new',
+  time: 'week'
 }).then((val)=>{
-  for(let i = 0;i<1;i++){
-    let title  = val[i].title;
-    let id = val[i].id;
-    r.getSubmission(id).comments.then((com)=>{
-      console.log(com[1]);
-      // for(let i = 0;i<com.length;i++){
-      //   console.log("=> "+com[i].body);
-      //   console.log("-------------------------------");
-      // }
-    });
-    console.log(`Title: ${title}, ID: ${id}`);
+  if(typeof(val[0])==='undefined'){
+    console.log("Nothing new.");
+  }else{
+    for(let i = 0;i<1;i++){
+      let title  = val[i].title;
+      let id = val[i].id;
+      // r.getSubmission(id).comments.then((com)=>{
+      //   console.log(com[1]);
+      //   // for(let i = 0;i<com.length;i++){
+      //   //   console.log("=> "+com[i].body);
+      //   //   console.log("-------------------------------");
+      //   // }
+      // });
+      console.log(`Title: ${title}, ID: ${id}`);
+    }
   }
-  
 });
---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+  dev:{
+    userAgent: "DLCOnoobGhost",
+    clientId: "NQ_Q0Jcy_KstnQ",
+    clientSecret: "6HJJAPfbr7C3VLoG7jwXbWH2n0I",
+    refreshToken: "70808853-dMtva1BwQ-TU4a9KL1YboRARXuE"
+  }
+};
+module.exports = config;
